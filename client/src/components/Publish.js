64c81fe0ec43fff995bd9c81d2fd1e76d30432af
Pierre Guerineau <polemoflora@gmail.com>
2022-06-06 16:46:40 -0400

    save draft button created

--- client/src/components/Publish.js
+++ client/src/components/Publish.js
@@ -2,53 +2,92 @@ import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
 import Embed from "@editorjs/embed";
+import { useState, useEffect } from "react";
 import ImageTool from "@editorjs/image";
 const LinkTool = require("@editorjs/link");
 const SimpleImage = require("@editorjs/simple-image");
 
-
-
-const editor = new EditorJS({
-  holder: "editor",
-  tools: {
-    header: {
-      class: Header,
-      config: {
-        placeholder: "Enter a header",
-        levels: [1, 2, 3, 4],
-        defaultLevel: 1,
-      },
-      inlineToolbar: ["link"],
-    },
-    list: {
-      class: List,
-      inlineToolbar: true,
-    },
-    image: SimpleImage,
-    embed: Embed,
-    linkTool: {
-      class: LinkTool,
-      config: {
-        endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching,
-      },
-    },
-  },
-  data: {},
-  onReady: () => {console.log('Editor.js is ready to work!')},
-  onChange: (api, event) => {
-    console.log('Now I know that Editor\'s content changed!', event)
-  },
-  placeholder: 'What is going on?',
-
-
-});
-
-
-
 const Publish = () => {
+  // const [currentArticle, setCurrentArticle] = useState(null);
+  let currentArticle;
+  const [savedArticle, setSavedArticle] = useState(null);
+
+
+  const editor = new EditorJS({
+    holder: "editor",
+    tools: {
+      header: {
+        class: Header,
+        config: {
+          placeholder: "Enter a header",
+          levels: [1, 2, 3, 4],
+          defaultLevel: 1,
+        },
+        inlineToolbar: ["link"],
+      },
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+      image: SimpleImage,
+      embed: Embed,
+      linkTool: {
+        class: LinkTool,
+        config: {
+          endpoint: "http://localhost:8008/fetchUrl", // Your backend endpoint for url data fetching,
+        },
+      },
+    },
+    data: {savedArticle},
+    onReady: () => {
+      console.log("Editor.js is ready to work!");
+    },
+    onChange: (api, event) => {
+      console.log("Now I know that Editor's content changed!", event);
+      editor
+        .save()
+        .then((outputData) => {
+          console.log("Article data: ", outputData);
+          // setCurrentArticle(outputData);
+          currentArticle = outputData;
+          // console.log(currentArticle);
+        })
+        .catch((error) => {
+          console.log("Saving failed: ", error);
+        });
+    },
+    placeholder: "What is going on?",
+  });
+
+  console.log(currentArticle);
+
+
+  const SaveDraft = (e) => {
+    e.preventDefault();
+    fetch("/api/add-reservation", {
+      method: "POST",
+      body: JSON.stringify(currentArticle),
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        const { status, message, data } = json;
+
+        if (status === 200) {
+          setSavedArticle(json.data);
+        } else {
+          alert(message);
+        }
+      });
+  };
+
   return (
     <div>
-      <div  id="editor"></div>
+      <div id="editor"></div>
+      <button onClick={() => SaveDraft()}>save draft</button>
     </div>
   );
 };
